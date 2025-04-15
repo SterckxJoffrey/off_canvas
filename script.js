c7b550cc94bcbcefb743100aa08597c8794dd27d
Joffrey Sterckx <147731791+SterckxJoffrey@users.noreply.github.com>
2025-04-15 12:19:36 +0200

    feat: bascule visibilité menu off-canvas avec JS

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-
 const toggleBtn = document.getElementById('toggle');
 const content = document.getElementById('off_canvas');
+const closeImg = document.getElementById('close_img');
 
 toggleBtn.addEventListener('click', () => {
-content.classList.toggle('hidden');
+  content.classList.toggle('active');
 });
 
+closeImg.addEventListener('click', () => {
+  content.classList.remove('active');
+});
